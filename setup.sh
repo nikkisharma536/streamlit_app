eed1d4ebfdefba13a5c5046518ce8de59e59fc88
nikkisharma536 <43667848+nikkisharma536@users.noreply.github.com>
2020-06-24 12:23:47 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo “\
 [general]\n\
-email = \”your-email@domain.com\”\n\
+email = \”nikkisharm536a@domain.com\”\n\
 “ > ~/.streamlit/credentials.toml
 echo “\
 [server]\n\
