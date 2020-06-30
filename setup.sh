cbb4627c17b194d876c20b302d9094d12d59cda8
nikkisharma536 <43667848+nikkisharma536@users.noreply.github.com>
2020-06-30 21:24:48 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"nikkisharma536@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
